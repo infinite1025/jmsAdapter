bdef258ab04910bf2da093b93af3d0ed3dd92a98
Javier Quintana <jalex.quintana@gmail.com>
2020-10-05 17:56:23 -0500

    Commit inicial

---
+++ helloworld-mdb/src/main/java/org/jboss/as/quickstarts/mdb/HelloWorldQueueMDB.java
@@ -0,0 +1,58 @@
+package org.jboss.as.quickstarts.mdb;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.CreateException;
+import javax.ejb.EJBException;
+import javax.ejb.MessageDriven;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.MessageProducer;
+import javax.jms.Queue;
+import javax.jms.QueueSession;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import org.jboss.ejb3.annotation.Pool;
+import org.jboss.ejb3.annotation.ResourceAdapter;
+import org.jboss.logging.Logger;
+
+
+/**
+ * A simple MDB (Message Driven Bean) that asynchronously receives and processes the
+ * messages that are sent to the JMS Provider.
+ * Pre-requisites : jboss-ejb3.xml is mandatory which needs to be packaged along with this code as war/ear/jar.
+ * @author Abu Davis
+ */
+ 
+ 
+public class HelloWorldQueueMDB implements MessageListener {
+
+
+	private final static Logger LOGGER = Logger.getLogger(HelloWorldQueueMDB.class.toString());
+				
+  	public void onMessage(Message rcvMessage) {
+		TextMessage msg = null;
+		try {
+			if (rcvMessage instanceof TextMessage) {
+				msg = (TextMessage) rcvMessage;
+				LOGGER.info("Received Message from queue: " + msg.getText());
+			} else {
+				LOGGER.info("Message of wrong type: "
+						+ rcvMessage.getClass().getName());
+			}
+		} catch (JMSException e) {
+			LOGGER.info("interrupted");
+			throw new RuntimeException(e);
+			
+		}
+	}
+
+}
---
+++ helloworld-mdb/src/main/java/org/jboss/as/quickstarts/servlet/HelloWorldMDBServletClient.java
@@ -0,0 +1,96 @@
+package org.jboss.as.quickstarts.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.annotation.Resource;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.jms.Topic;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ejb.Stateless;
+import java.util.UUID;
+import org.jboss.logging.Logger;
+
+/**
+ * A simple servlet 3 as client that sends several messages to a queue.
+ * 
+ * @author Abu Davis
+ */
+
+@WebServlet("/HelloWorldMDBServletClient")
+public class HelloWorldMDBServletClient extends HttpServlet {
+
+	private final static Logger LOGGER = Logger.getLogger(HelloWorldMDBServletClient.class.toString());
+
+	private static final long serialVersionUID = -8314035702649252239L;
+
+	private static final int MSG_COUNT = 5;
+
+	@Resource(mappedName = "java:/jboss/ConnectionFactory")
+	private ConnectionFactory connectionFactory;
+
+	@Resource(mappedName = "java:/queue/ticketOrderQueue")
+	private Queue queue;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		Connection connection = null;
+		out.write(
+				"<h1>Quickstart: Example demonstrates the use of <strong>JMS 1.1</strong> and <strong>EJB 3.1 Message-Driven Bean using IBM MQ JMS Provider </strong> in JBoss Enterprise Application 6.x/7.x</h1>");
+		try {
+			Destination destination;
+			destination = queue;
+			LOGGER.info("<p>Sending messages to <em>" + destination + "</em></p>");
+			out.write("<p>Sending messages to <em>" + destination + "</em></p>");
+			connection = connectionFactory.createConnection();
+			Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			MessageProducer messageProducer = session.createProducer(destination);
+			connection.start();
+			out.write("<h2>Following messages will be send to the destination:</h2>");
+			TextMessage message = session.createTextMessage();
+			for (int i = 0; i < MSG_COUNT; i++) {
+				message.setText("This is message " + (i + 1));
+				messageProducer.send(message);
+				LOGGER.info("Sent Message (" + i + "): " + message.getText() + "</br>");
+				out.write("Sent Message (" + i + "): " + message.getText() + "</br>");
+			}
+			out.write(
+					"<p><i>Go to your JBoss Application Server console or Server log to see the result of messages processing</i></p>");
+
+		} catch (JMSException e) {
+			e.printStackTrace();
+			out.write("<h2>A problem occurred during the delivery of this message</h2>");
+			out.write("</br>");
+			out.write(
+					"<p><i>Go your the JBoss Application Server console or Server log to see the error stack trace</i></p>");
+		} finally {
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (JMSException e) {
+					e.printStackTrace();
+				}
+			}
+			if (out != null) {
+				out.close();
+			}
+		}
+	}
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}
+
+}
